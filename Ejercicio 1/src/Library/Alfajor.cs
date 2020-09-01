2e8e0b15adfeab74d925b29c8aebe1a69184f0e6
TyMata <alvaromanuel.machado@correo.ucu.edu.uy>
2020-09-01 20:15:25 -0300

    Cambio de codigo
    Se agrego la clase Convertidor
    Se agrego "PrecioAlfajor" a la clase Alfajor
    Se removio "ConvertirAPesos" de la clase Kiosco

--- Ejercicio 1/src/Library/Alfajor.cs
+++ Ejercicio 1/src/Library/Alfajor.cs
@@ -4,12 +4,17 @@ namespace Expert_SRP
 {
     public class Alfajor 
     {
-        public Alfajor(double precioMasa, double precioDulce)
+        public Alfajor(Double precioMasa, Double precioDulce)
         {
             this.PrecioDulce = precioDulce;
             this.PrecioMasa = precioMasa;
         }
         public Double PrecioDulce {get;set;}
         public Double PrecioMasa {get; set;}
+
+        public Double PrecioAlfajor()
+        {
+            return this.PrecioDulce + this.PrecioMasa;
+        }
     }
 }
