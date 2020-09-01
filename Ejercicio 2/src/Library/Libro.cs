e915c1cb2127d3101f373321168231a44926c48c
TyMata <alvaromanuel.machado@correo.ucu.edu.uy>
2020-09-01 20:45:50 -0300

    ComentariosEj2
    Se agregaron comentarios al Ej2

--- Ejercicio 2/src/Library/Libro.cs
+++ Ejercicio 2/src/Library/Libro.cs
@@ -8,8 +8,8 @@ namespace SRP
         public string Titulo { get ; }
         public string Autor { get ; }
         public string Codigo { get ;  }
-        public string SectorBiblioteca { get ; set; }
-        public string EstanteBiblioteca { get ; set; }
+        public string SectorBiblioteca { get ; set; }// No cumple con el patron Expert ya que esta responsabilidad no le pertenece al libro le pertenece a otra clase
+        public string EstanteBiblioteca { get ; set; }// No cumple con el patron Expert ya que esta responsabilidad no le pertenece al libro le pertenece a otra clase
 
         public Libro(String titulo, String autor, String codigo)
         {
@@ -18,7 +18,7 @@ namespace SRP
             this.Codigo = codigo;
         }
 
-        public void AlmacenarLibro(String sector, String estante)
+        public void AlmacenarLibro(String sector, String estante) //Esto deberia ir en otra clase ya que no cumple con el patron SRP
         {
             this.SectorBiblioteca = sector;
             this.EstanteBiblioteca = estante;
